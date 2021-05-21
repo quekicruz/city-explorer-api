e27b69fdbe9afb763981c8edaa926d3fb4028d8d
Quentin Cruz <napu0515@gmail.com>
2021-05-20 21:18:15 -0700

    some of lab 09

--- server.js
+++ server.js
@@ -11,56 +11,97 @@ app.use(cors());
 // let weather = require('./data/weather.json')
 
 const PORT = process.env.PORT || 3050;
-const WEATHER_API = process.env.WEATHER_API_KEY;
-const MOVIE_API = process.env.MOVIE_API_KEY;
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+const MOVIE_API_KEY = process.env.MOVIE_API_KEY;
 
 
-class Forecast{
-  constructor(date,description) {
-    this.date = date;
-    this.description= description;
-  }
-}
+// const getWeather = require('./lib/weather.js')
+// const getMovies = require('./lib/movies.js')
 
-https://api.themoviedb.org/3/movie/550?api_key=974c6d2fe59ecbaf90dea41522c600bf
 
 app.get('/weather', getWeather);
 
 async function getWeather (request,response) {
-  const searchQuery = request.query.query;
 
-  const weatherUrl = `http://api.weatherbit.io/v2.0/current?key=4a167f382c70437cab0e8d65a553b68d`
-  console.log(weatherUrl);
-  let weatherResponse = await axios.get(weatherUrl);
-  console.log(weatherResponse.data);
-  response.send('Here is the weather');
-}
+  const lat = request.query.lat;
+  const lon = request.query.lon
+  
+  const weatherUrl = `http://api.weatherbit.io/v2.0/current?lat=${lat}&lon=${lon}&key=${WEATHER_API_KEY}`
+  let weatherResponse = await axios.get(weatherUrl)
+  console.log(weatherResponse);
+  response.json(weatherResponse.data)
+  
+  try{
+    
+    const allForecast = weatherResponse.data.data.map(day => new Forecast(day)); 
+    response.send(allForecast);
+    response.json(allForecast);
+    
+  } catch (error) {
+    Error(error, response);
+  }
+  
+};
+  
+  
+  // .then(response => {
+  // })
+  // .catch(error => {
+  //   { console.log("Error, can not be displayed")}
+  // })
+  
+  
+
 
 
 app.get('/movies',getMovies);
 
 async function getMovies (request, response ) {
   
-  const movieUrl = `https://api.themoviedb.org/3/movie/550?api_key=974c6d2fe59ecbaf90dea41522c600bf`
-
-   let movieResponse = await axios.get(movieUrl);
-   console.log(movieResponse.data);
-   response.json(movieResponse.data)
-   console.log(request.query)
   
-  //  const newMovieArray = movieResponse.data.filter(movie => {
-  //   movie.name.includes(request.query.name)
-  //  })
-  //  console.log(newMovieArray);
-  //  response.json(newMovieArray);
-}
+  const searchM = request.query.search 
+  
+  const movieUrl = `https://api.themoviedb.org/3/search/movie?api_key=${MOVIE_API_KEY}&query=${searchM}`
+  
+  
+  const movieResponse = await axios.get(movieUrl)
+  console.log(movieResponse.data);
+  response.json(movieResponse.data);
+  
+  // .then(response => {
+  //   console.log(request.query);
+    
+  // })
+  // .catch(error=> {
+    //   { console.log("Error, can not be displayed")}
+    // })
+    
+    //  const newMovieArray = movieResponse.data.filter(movie => {
+      //   movie.name.includes(request.query.name)
+      //  })
+      //  console.log(newMovieArray);
+      //  response.json(newMovieArray);
+    }
+    
+    app.get('*', (request, response) => {
+      response.status(500).send('Something went wrong... Wrong route sent!');
+    });
 
-class Movies{
-  constructor(){
 
-  }
-}
+    class Movies{
+      constructor(){
+        
+      }
+    }
+    
+    class Forecast{
+      constructor(day) {
+        this.date = day.datetime;
+        this.description= `Low of ${day.low_temp} and high of ${day.high_temp}, with ${day.weather.description}`
+      }
+    }
 
 
-
+    
+    
 app.listen(PORT, () => console.log('Server is running'))
